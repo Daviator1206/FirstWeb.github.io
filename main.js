254dab4458144b1130830e074fee0110ffdbbad1
krishna <rautmurti@gmail.com>
2024-07-07 21:56:45 +0530

    add database mongodb

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const port = 5500;
 const path = require("path")
 const app = express()
 const userRoute = require("./routes/user")
+
+mongoose.connect("mongodb://127.0.0.1:27017/hackerEarth")
+.then(()=>{console.log("MongoDb successfully connected!")})
+.catch((err)=>{console.log("something went wrong: ",err)})
+
 app.use(express.urlencoded({extended:false}))
 
 app.set("view engine", "ejs" )
