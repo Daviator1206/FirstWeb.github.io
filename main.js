2ac5f798d5eabc672278163e5deca27db07a6961
krishna <rautmurti@gmail.com>
2024-07-07 16:30:21 +0530

    add signin page

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const express = require("express")
-const port = 8000;
+const port = 5500;
 const path = require("path")
 const app = express()
-
+const userRoute = require("./routes/user")
 app.use(express.urlencoded({extended:false}))
 
 app.set("view engine", "ejs" )
@@ -12,6 +12,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get("/",(req, res)=>{
     res.render("home")
 })
+app.use("/user",userRoute)
 
 app.listen(port, ()=>{
     console.log(`Server started at port number ${port}`)
---
+++ routes/user.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const router = express.Router()
+
+router.get("/signin", (req, res)=>{
+    return res.render("signin")
+})
+router.get("/signup", (req, res)=>{
+    return res.render("signup")
+})
+
+
+
+module.exports = router;
